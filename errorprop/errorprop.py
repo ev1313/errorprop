eacbef7a3e1903c4cb43fe8fe3b9ab47caf65244
Tim Blume <tb@ciphron.de>
2019-07-08 16:45:08 +0200

    added and renamed methods, refactored

--- errorprop/errorprop.py
+++ errorprop/errorprop.py
@@ -1,50 +1,30 @@
 import sympy as sy
 import numpy as np
-import uncertainties as uc
 
 """
+    This method calculates the given equation and does a gaussian error propagation for a single datapoint.
+
     Parameters
     ==========
     equation : string
         symbol string to be used in equation, needs to be sympy syntax (I is imaginary unit for example)
     datadict: dictionary
         the keys of this dictionary represent the symbols used in the equation string.
-        each key can either be a 2-tuple or a list of 2-tuples or 3-tuples.
-
-        a 2-tuple consists of the value and the error/deviation. (the error can be 0 if none is necessary)
-        the same goes for the elements of a list.
-
-        if lists are used, all need to be of the same length!
+        each key has to be a 2-tuple in the format (value, deviation)
 """
-def calc_errorprop(equation, datadict):
-    eq_expr = sy.sympify(equation)
+def calc_gaussian_errorprop_single(equation, datadict):
     err_expr = 0
     calc = {}
-    data_list_len = 0
     for n, data in datadict.items():
         if isinstance(data, tuple):
             assert(len(data)==2, "{} tuple does not have two elements!".format(n))
-
-        elif isinstance(data, list):
-            if not data_list_len == 0:
-                data_list_len = len(data)
-            else:
-                assert(data_list_len == len(data), "data lists have inconsistent lengths! {} has length {} but should have {}".format(n, len(data), data_list_len))
-
-            i = 0
-            for d in data:
-                assert(isinstance(d, tuple), "data list {} {} contains not a tuple at position {}!".format(d,i))
-                assert(len(d)==2, "{} tuple in data list at position {} does not have two elements!".format(n, d, i))
-                i+=1
-
         else:
-            assert(False, "{} data is not tuple or list!".format(n))
-            
+            assert(False, "{} data is not a 2-tuple!".format(n))
         # generate derivates
         symbol = sy.Symbol(n)
         symbol_err = sy.Symbol("sigma_{}".format(n))
-        symbol_der = sy.diff(eq_expr,symbol)
-        symbol_der = symbol_der * symbol_err 
+        symbol_der = sy.diff(equation,symbol)
+        symbol_der = symbol_der * symbol_err
 
         calc[n] = (symbol, symbol_der, symbol_err)
         err_expr = err_expr + symbol_der**2
@@ -52,7 +32,7 @@ def calc_errorprop(equation, datadict):
     err_expr = sy.simplify(sy.sqrt(err_expr))
     err_expr = sy.powdenest(err_expr, force=True)
 
-    nominal = eq_expr
+    nominal = equation
     deviation = err_expr
 
     # calculate nominal + deviate values
@@ -61,16 +41,53 @@ def calc_errorprop(equation, datadict):
         deviation = deviation.subs(calc[n][0], str(d[0]))
         deviation = deviation.subs(calc[n][2], str(d[1]))
 
-    try:
-        nominal = float(nominal)
-    except:
-        assert(False, "Could not finish nominal evalution due to missing values, stopped with equation {}".format(str(nominal)))
-        
-    try:
-        deviation = float(deviation)
-    except:
-        assert(False, "Could not finish deviation evalution due to missing values, stopped with equation {}".format(str(deviation)))
+    return nominal, deviation
 
-    # cast to uncertainties
-    return uc.ufloat(nominal,deviation)
+"""
+    This method calculates a gaussian error propagation similar to calc_gaussian_errorprop_single, however allows lists in the datadict also.
 
+    Parameters
+    ==========
+    equation : sympy equation
+        the equation to be used
+    datadict: dictionary
+        the keys of this dictionary represent the symbols used in the equation string.
+        each key has to be a 2-tuple or a list of 2-tuples all in the format (value, deviation)
+
+        all lists in the datadict have to have the same length.
+
+    Returns
+    =======
+        a list with the length of the lists in the datadict is returned.
+"""
+def calc_gaussian_errorprop_multi(equation, datadict):
+    results = []
+    data_list_len = 0
+    for n, data in datadict.items():
+        if isinstance(data, list):
+            if data_list_len == 0:
+                data_list_len = len(data)
+            else:
+                assert(data_list_len == len(data), "data lists have inconsistent lengths! {} has length {} but should have {}".format(n, len(data), data_list_len))
+
+            i = 0
+            for d in data:
+                assert(isinstance(d, tuple), "data list {} {} contains not a tuple at position {}!".format(n, d, i))
+                assert(len(d)==2, "{} tuple in data list at position {} does not have two elements!".format(n, d, i))
+                i+=1
+        elif isinstance(data, tuple):
+            assert(len(data)==2, "{} tuple does not have two elements!".format(n))
+        else:
+            assert(False, "{} data is not a 2-tuple!".format(n))
+
+    results = []
+    for i in range(data_list_len):
+        temp_data = {}
+        for n, data in datadict.items():
+            if isinstance(data, list):
+                temp_data[n] = data[i]
+            else:
+                temp_data[n] = data
+        results.append(calc_gaussian_errorprop_single(equation, temp_data))
+
+    return results
--- tests/test_errorprop.py
+++ tests/test_errorprop.py
@@ -1,11 +1,34 @@
-from errorprop.errorprop import calc_errorprop
+from errorprop.errorprop import calc_gaussian_errorprop_single, calc_gaussian_errorprop_multi
+import sympy as sy
 
-def test_errorprop1():
+def test_errorprop_single1():
     datadict = {
             "m": (1., 0.1),
             "c": (28E3, 0.0)
             }
-    E = calc_errorprop("m*c**2", datadict)
-    
-    assert(round(E.n,6)==7.84e8)
-    assert(round(E.std_dev,6)==7.84e7)
+    E = calc_gaussian_errorprop_single(sy.sympify("m*c**2"), datadict)
+
+    assert(round(E[0],6)==7.84e8)
+    assert(round(E[1],6)==7.84e7)
+
+def test_errorprop_multi1():
+    datadict = {
+            "m": [
+                (1., 0.1),
+                (2., 0.01),
+                (3., 0.15),
+                (4., 0.0),
+            ],
+            "c": (28E3, 0.0)
+            }
+    E = calc_gaussian_errorprop_multi(sy.sympify("m*c**2"), datadict)
+
+    assert(round(E[0][0], 5) == 784000000.000000)#, 78400000.0000000))
+    assert(round(E[1][0], 5) == 1568000000.00000)#, 7840000.00000000))
+    assert(round(E[2][0], 5) == 2352000000.00000)#, 117600000.000000))
+    assert(round(E[3][0], 5) == 3136000000.00000)#, 0))
+    assert(round(E[0][1], 5) == 78400000.0000000)
+    assert(round(E[1][1], 5) == 7840000.00000000)
+    assert(round(E[2][1], 5) == 117600000.000000)
+    assert(round(E[3][1], 5) == 0)
+
